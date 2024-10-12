0ff5d0de1f264bbe4c95d03ab5f1b2c8a3906420
Mike Dean <miketraindoc@gmail.com>
2024-10-12 11:19:15 -0600

    Consent document example markdown

--- experimental/defaults.py
+++ experimental/defaults.py
@@ -6,4 +6,4 @@ OPENAI_API_KEY = os.getenv("NEWKEY")
 default_embedding_model = OpenAIEmbeddings(model="text-embedding-3-small", api_key=OPENAI_API_KEY)
 default_location = ":memory:"
 default_url = "http://localhost:6333"
-default_llm = ChatOpenAI(model="gpt-4o", api_key=OPENAI_API_KEY, streaming=True)
+default_llm = ChatOpenAI(model="gpt-4o", api_key=OPENAI_API_KEY, streaming=True, temperature=0)
