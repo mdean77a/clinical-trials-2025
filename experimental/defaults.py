202262e970906f5d7a0b589958c7492685db91eb
Mike Dean <miketraindoc@gmail.com>
2025-06-09 14:48:15 -0600

    Getting rid of unnecessary files

--- experimental/defaults.py
+++
@@ -1,9 +0,0 @@
-# Defaults
-from langchain_openai.embeddings import OpenAIEmbeddings
-from langchain_openai.chat_models import ChatOpenAI
-import os
-OPENAI_API_KEY = os.getenv("NEWKEY")
-default_embedding_model = OpenAIEmbeddings(model="text-embedding-3-small", api_key=OPENAI_API_KEY)
-default_location = ":memory:"
-default_url = "http://localhost:6333"
-default_llm = ChatOpenAI(model="gpt-4o", api_key=OPENAI_API_KEY, streaming=True, temperature=0)
